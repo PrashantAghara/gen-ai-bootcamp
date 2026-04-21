5c32c40e6d5c17fc396d6a0707228d7bfa2cdc5d
Prashant Aghara <prashant.aghara2111@gmail.com>
2026-04-21 15:21:04 +0530

    Simple GenAI App

---
+++ langchain/simple_gen_ai_app/app.py
@@ -0,0 +1,33 @@
+import os
+import streamlit as st
+from dotenv import load_dotenv
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+os.environ['LANGCHAIN_API_KEY'] = os.getenv("LANGCHAIN_API_KEY")
+os.environ['LANGCHAIN_TRACING_V2'] = "true" 
+os.environ['LANGCHAIN_PROJECT'] = os.getenv("LANGCHAIN_PROJECT")
+
+
+## Prompt Template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please respond to the question asked"),
+        ("user", "Question : {question}")
+    ]
+)
+
+## Streamlit app
+st.title("Langchain Demo with Llama2")
+input_text = st.text_input("What question you have in mind?")
+
+## Call Ollama models
+llm = OllamaLLM(model="gemma:2b")
+output_parser = StrOutputParser()
+chain = prompt|llm|output_parser
+
+if input_text:
+    st.write(
+    chain.invoke({"question": input_text}))
